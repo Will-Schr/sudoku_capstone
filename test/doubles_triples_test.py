7c1aeee79a3897bd2d0a42403011f19d0d7ab8a5
Will-Schr <will.a.schreiner@gmail.com>
2024-03-26 19:33:55 -0400

    added hidden doubles tests

--- test/doubles_triples_test.py
+++ test/doubles_triples_test.py
@@ -164,19 +164,21 @@ class Test_naked_doubles(unittest.TestCase):
         test_board.naked_pairs_v()
         self.assertEqual(test_board.test_out_table(),solution_board.test_out_table())
 
-# class Test_hidden_doubles(unittest.TestCase):
-#     """Tests matches_h finding hidden doubles"""
-#     def test_hidden_double_1(self):
-#         """Test 1 of hidden double"""
-#         test_row = board([square(pos_in=[1,2,3]), square(pos_in=[1,2,3]), square(pos_in=[3,4]), square(pos_in=[3,5])])
-#         solution_row = board([square(pos_in=[1,2]), square(pos_in=[1,2]), square(pos_in=[3,4]), square(pos_in=[3,5])])
-#         self.assertEqual(test_row.test_out_row(),solution_row.test_out_row())
+class Test_hidden_doubles(unittest.TestCase):
+    """Tests matches_h finding hidden doubles"""
+    def test_hidden_double_1(self):
+        """Test 1 of hidden double"""
+        test_row = board([[square(pos_in=[1,2,3]), square(pos_in=[1,2,3]), square(pos_in=[3,4]), square(pos_in=[3,5])]])
+        solution_row = board([[square(pos_in=[1,2]), square(pos_in=[1,2]), square(pos_in=[3,4]), square(pos_in=[3,5])]])
+        test_row.hidden_pairs_h()
+        self.assertEqual(test_row.test_out_table(),solution_row.test_out_table())
 
-#     def test_hidden_double_2(self):
-#         """Test 2 of hidden double"""
-#         test_row = board([square(pos_in=[1,3,5]), square(pos_in=[5,6,7]), square(pos_in=[1,3,7]), square(pos_in=[8,9])])
-#         solution_row = board([square(pos_in=[1,3]), square(pos_in=[5,6,7]), square(pos_in=[1,3]), square(pos_in=[8,9])])
-#         self.assertEqual(test_row.test_out_row(),solution_row.test_out_row())
+    def test_hidden_double_2(self):
+        """Test 2 of hidden double"""
+        test_row = board([[square(pos_in=[1,3,5]), square(pos_in=[5,6,7]), square(pos_in=[1,3,7]), square(pos_in=[8,9])]])
+        solution_row = board([[square(pos_in=[1,3]), square(pos_in=[5,6,7]), square(pos_in=[1,3]), square(pos_in=[8,9])]])
+        test_row.hidden_pairs_h()
+        self.assertEqual(test_row.test_out_table(),solution_row.test_out_table())
 
 # class Test_naked_triples(unittest.TestCase):
 #     """Tests matches_h finding naked triples"""
